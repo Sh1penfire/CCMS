8030a23db2732be814fb93ee896b12831906dbc8
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-07 21:04:51 +1100

    Update rules.js

--- scripts/content/rules.js
+++ scripts/content/rules.js
@@ -1,39 +1,22 @@
-//Events.on(WorldLoadEvent, e => {
-//Vars.state.rules.buildCostMultiplier = 0.8;
-//Vars.state.rules.deconstructRefundMultiplier = 1;
-//Vars.state.rules.enemyLights = false;
-//Vars.state.rules.unitAmmo = true;
-//#});
-const CCMSBrulechange = newEffect(20, e => {
-    Draw.color(Color.yellow, Color.orange, e.fin()); //color goes from yellow to orange
-    Lines.stroke(e.fout() * 2); //line thickness goes from 2 to 0
-    Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
-});
+/*Events.on(WorldLoadEvent, e => {
+Vars.state.rules.buildCostMultiplier = 0.8;
+Vars.state.rules.deconstructRefundMultiplier = 1;
+Vars.state.rules.enemyLights = false;
+Vars.state.rules.unitAmmo = true;
+});*/
 
-//create the block type
-const ccmsb = extendContent(Block, "CCMSB", {
-    //override the method to build configuration
-    buildConfiguration(tile, table){
-        table.addImageButton(Icon.hammer, Styles.clearTransi, run(() => {
-            //configure the tile to signal that it has been pressed (this sync on client to server)
-            tile.configure(0)
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-    },
-
-    //override configure event
-    configured(tile, value){
-        //make sure this silo has the items it needs to change config
-        if(tile.entity.cons.valid()){
-            //make this effect occur at the tile location
-            Effects.effect(CCMSBrulechange, tile)
-
-            //Redefine variables
+Events.on(WorldLoadEvent, b => {
+    Vars.ui.showCustomConfirm(
+        "[]Switch on [#8f00ff][] Campaign mode?[]","[#8f00ff]Campaign mode[] will enable certain reatrues and lock some stats in place. It decreases build costs and changes lighting rules a bit.\n[accent]Do you want to Campaign mode?[]",
+        "No",
+        "Yes",
+        ()=>{
+            print("[accent]Campaign mode not enabled[]"
+        )},
+        ()=>{  
             Vars.state.rules.buildCostMultiplier = 0.8;
             Vars.state.rules.deconstructRefundMultiplier = 1;
             Vars.state.rules.enemyLights = false;
             Vars.state.rules.unitAmmo = true;
-            //triggering consumption makes it use up the items it requires
-            tile.entity.cons.trigger()
-        }
-    }
-})
+        });
+});
