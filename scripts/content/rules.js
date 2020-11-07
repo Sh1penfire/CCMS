cd9b431a52735e9b77f6fcfb29e1893ccb917ff7
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-07 21:07:52 +1100

    Update rules.js

--- scripts/content/rules.js
+++ scripts/content/rules.js
@@ -5,7 +5,7 @@ Vars.state.rules.enemyLights = false;
 Vars.state.rules.unitAmmo = true;
 });*/
 
-Events.on(WorldLoadEvent, b => {
+Events.on(WorldLoadEvent, e => {
     Vars.ui.showCustomConfirm(
         "[]Switch on [#8f00ff][] Campaign mode?[]","[#8f00ff]Campaign mode[] will enable certain reatrues and lock some stats in place. It decreases build costs and changes lighting rules a bit.\n[accent]Do you want to Campaign mode?[]",
         "No",
