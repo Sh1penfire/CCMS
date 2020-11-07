2f6102de0444f33377b1ea60ee2b8cf7c160c169
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-07 16:57:17 +1100

    Update rules.js

--- scripts/content/rules.js
+++ scripts/content/rules.js
@@ -1,4 +1,6 @@
+Events.on(WorldLoadEvent, e => {
 Vars.state.rules.buildCostMultiplier = 0;
 Vars.state.rules.deconstructRefundMultiplier = 1;
 Vars.state.rules.enemyLights = false;
 Vars.state.rules.unitAmmo = true;
+});
