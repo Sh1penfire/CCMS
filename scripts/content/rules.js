0696fc4b90f6485fa15105b652e45d95d3e07099
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-07 21:11:45 +1100

    Update rules.js

--- scripts/content/rules.js
+++ scripts/content/rules.js
@@ -5,10 +5,7 @@ Vars.state.rules.enemyLights = false;
 Vars.state.rules.unitAmmo = true;
 });*/
             print("Asking player for enabling)"
-            Vars.state.rules.buildCostMultiplier = 1;
-            Vars.state.rules.deconstructRefundMultiplier = 0.5;
-            Vars.state.rules.enemyLights = true;
-            Vars.state.rules.unitAmmo = false;
+
 Events.on(WorldLoadEvent, e => {
     Vars.ui.showCustomConfirm(
         "[]Switch on [#8f00ff][] Campaign mode?[]","[#8f00ff]Campaign mode[] will enable certain reatrues and lock some stats in place. It decreases build costs and changes lighting rules a bit.\n[accent]Do you want to Campaign mode?[]",
@@ -16,6 +13,10 @@ Events.on(WorldLoadEvent, e => {
         "Yes",
         ()=>{
             print("[accent]Campaign mode not enabled[]"
+            Vars.state.rules.buildCostMultiplier = 1;
+            Vars.state.rules.deconstructRefundMultiplier = 0.5;
+            Vars.state.rules.enemyLights = true;
+            Vars.state.rules.unitAmmo = false;
         )},
         ()=>{  
             Vars.state.rules.buildCostMultiplier = 0.8;
