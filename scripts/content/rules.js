0e47c400d97c4f25312e85e8a5f8efe9dc03820e
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-07 21:26:11 +1100

    Update rules.js

--- scripts/content/rules.js
+++ scripts/content/rules.js
@@ -10,14 +10,14 @@ Events.on(ClientLoadEvent, b => {
         "[]Switch on [#8f00ff][] Campaign mode?[]","[#8f00ff]Campaign mode[] will enable certain reatrues and lock some stats in place. It decreases build costs and changes lighting rules a bit.\n[accent]Do you want to Campaign mode?[]",
         "No",
         "Yes",
-        ()=>{
+        (=>{
             print("[accent]Campaign mode not enabled[]")
             Vars.state.rules.buildCostMultiplier = 1;
             Vars.state.rules.deconstructRefundMultiplier = 0.5;
             Vars.state.rules.enemyLights = true;
             Vars.state.rules.unitAmmo = false;
         )},
-        ()=>{  
+        (=>{  
             Vars.state.rules.buildCostMultiplier = 0.8;
             Vars.state.rules.deconstructRefundMultiplier = 1;
             Vars.state.rules.enemyLights = false;
