87127689c96a97bb585412a129dd3bdcab510d33
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-07 23:41:25 +1100

    Delete rules.js

--- scripts/content/rules.js
+++
@@ -1,8 +0,0 @@
-/*
-Events.on(WorldLoadEvent, e => {
-Vars.state.rules.buildCostMultiplier = 0.8;
-Vars.state.rules.deconstructRefundMultiplier = 1;
-Vars.state.rules.enemyLights = false;
-Vars.state.rules.unitAmmo = true;
-});
-*/
