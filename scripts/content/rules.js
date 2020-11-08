56bf682c57a73973e77b8895257066e884d0b471
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-08 21:57:04 +1100

    Update rules.js

--- scripts/content/rules.js
+++ scripts/content/rules.js
@@ -1,5 +1,32 @@
 // obligatory comment here
-Events.on(WorldLoadEvent, e => {
+
+var enabledCM = 0
+
+const waveeffect1 = new Effect(20, e => {
+    
+    //color goes from white to light gray
+    Draw.color(
+        Color.white, 
+        Color.lightGray, 
+        e.fout()
+    );
+    
+    //line thickness goes from 2 to 0
+    Lines.stroke(e.fout() * 2); 
+    
+    //draw a circle whose radius goes from 0 to 25
+    Lines.circle(
+        e.x, 
+        e.y, 
+        e.fin() * 25
+    ); 
+});
+
+
+Events.on(ClientLoadEvent, b  => {
+    print(b);
+    print("above");
+    //enabledCM = "null";
     print("Asking player for enabling");
     Vars.ui.showCustomConfirm(
         "Switch on [#86BAF9]Campaign mode?[]",
@@ -7,14 +34,51 @@ Events.on(WorldLoadEvent, e => {
         "No",
         "Yes",
         () => {
-            print("[accent]Campaign mode not enabled[]")
+            enabledCM = 1;  
+            print("Campaign mode enabled")
         },
-        () => {
-            print("[#00ff00]Campaign mode is enabled[]")
-            Vars.state.rules.buildCostMultiplier = 0.8;
+        () => {            
+            enabledCM = 0;
+            print("Campaign mode disabled")
+        }
+    );
+});
+
+
+Events.on(WorldLoadEvent, e => {
+
+
+print("enabledCM is: " + enabledCM)
+
+    if(enabledCM == 1){
+            print("[#00ff00]Turning on campaign mode for world[]");
+            Vars.state.rules.buildCostMultiplier = 1;
+            Vars.state.rules.deconstructRefundMultiplier = 0.5;
+            Vars.state.rules.enemyLights = true;
+            Vars.state.rules.unitAmmo = false;
+    };
+    if(enabledCM == 0){
+            print("[#00ff00]Turning off campaign mode for world[]");
+            Vars.state.rules.buildCostMultiplier = 1;
             Vars.state.rules.deconstructRefundMultiplier = 1;
             Vars.state.rules.enemyLights = false;
             Vars.state.rules.unitAmmo = true;
-        }
-    );
+    };
+});
+
+
+Events.on(WorldLoadEvent, e => {
+    
+Vars.ui.showInfoText(
+    "Hello!", "Hello world!"
+);
+});
+
+
+Events.on(WaveEvent, e => {
+
+let x = Vars.player.x;
+let y = Vars.player.y;
+
+waveeffect1.at(x, y);
 });
